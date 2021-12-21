fb08f12436d62841f137239c9fe1c10fb3b7367a
Chriss <chriss713@gmail.com>
2021-12-21 19:25:17 +0100

    Added PUT request

--- src/test/java/trainingxyz/ApiTests.java
+++ src/test/java/trainingxyz/ApiTests.java
@@ -39,4 +39,20 @@ public class ApiTests {
         var response = given().body(body).when().post(endpoint).then();
         response.log().body();
     }
+
+    @Test
+    public void updateProduct() {
+        String endpoint = "http://127.0.0.1/api_testing/product/update.php";
+        String body = """
+                {
+                "id": 1000,
+                "name": "Water Bottle",
+                "description": "Blue water bottle. Holds 64 ounces.",
+                "price": 15,
+                "category_id": 3
+                }
+                """;
+        var response = given().body(body).when().put(endpoint).then();
+        response.log().body();
+    }
 }
