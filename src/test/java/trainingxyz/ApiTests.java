24b1d3bcfc7ed7b9e69817aad7845ce55aa7cf33
Chriss <chriss713@gmail.com>
2021-12-21 18:32:26 +0100

    Added first requests

---
+++ src/test/java/trainingxyz/ApiTests.java
@@ -0,0 +1,27 @@
+package trainingxyz;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class ApiTests {
+
+    @Test
+    public void getCategories() {
+        String endpoint = "http://127.0.0.1/api_testing/product/read.php";
+        var response = given().when().get(endpoint).then();
+        response.log().body();
+    }
+
+    @Test
+    public void getProduct(){
+        String endpoint = "http://127.0.0.1/api_testing/product/read_one.php";
+        var response =
+                given().
+                        queryParam("id", 2).
+                when().
+                        get(endpoint).
+                then();
+        response.log().body();
+    }
+}
